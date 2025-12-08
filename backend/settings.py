3f52d0de1ed2d34ab02c65a257989d2580f82b23
Oguz <erden.ouz@gmail.com>
2025-12-08 16:31:31 +0100

    Update frontend to call backend directly with Azure URLs

--- backend/settings.py
+++ backend/settings.py
@@ -70,6 +70,7 @@ ROOT_URLCONF = 'backend.urls'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
+    "http://wikiquote-frontend.germanywestcentral.azurecontainer.io:8080",
     "http://127.0.0.1:8080",
     "http://localhost:5500",  # VS Code Live Server
 ]
